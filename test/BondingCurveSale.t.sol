1bb844231e853dac8d68b9bea0a59793663a0d16
grc02 <georgi.chonkov27@gmail.com>
2023-11-08 19:38:44 +0200

    Refactored unit tests for 'BondingCurveSale' contract

--- test/BondingCurveSale.t.sol
+++ test/BondingCurveSale.t.sol
@@ -16,8 +16,10 @@ contract BondingCurveSaleTest is Test {
         owner = vm.addr(444);
 
         user1 = vm.addr(666);
+        vm.deal(user1, 1000 ether);
 
         user2 = vm.addr(999);
+        vm.deal(user2, 1000 ether);
 
         bannedUser = vm.addr(777);
 
@@ -30,116 +32,125 @@ contract BondingCurveSaleTest is Test {
         assertEq(sale.name(), "Token");
         assertEq(sale.symbol(), "TKN");
         assertEq(sale.pricePerToken(), 1 ether);
-        console2.log("'currentPrice': %s", sale.currentPrice() / 10 ** 18);
-        assertEq(sale.currentPrice(), 1 ether);
+        assertEq(sale.currentPrice(), sale.basePrice());
     }
 
     function testBuyPrice() public {
         uint256 tokenAmount = 4 ether;
         uint256 buyPrice = sale.buyPriceCalculation(tokenAmount);
-        console2.log("'buyPrice' for 4 tokens is %s", buyPrice);
+        assertEq(buyPrice, 12 ether);
+
+        buyPrice = sale.buyPriceCalculation(tokenAmount * 5 / 2);
+        assertEq(buyPrice, 60 ether);
+
+        buyPrice = sale.buyPriceCalculation(tokenAmount - 2 ether);
+        assertEq(buyPrice, 4 ether);
 
         buyPrice = sale.buyPriceCalculation(tokenAmount - 3 ether);
-        console2.log("'buyPrice' for 1 token is %s", buyPrice);
-        // 12000000000000000000
-        //  8000000000000000000
+        assertEq(buyPrice, 3 ether / 2);
     }
 
-    function testBuyFail() public {
+    function testBuy() public {
         uint256 tokenAmount = 5 ether;
         uint256 buyPrice = sale.buyPriceCalculation(tokenAmount);
-        console2.log("'buyPrice' for 5 tokens is %s", buyPrice);
-
-        vm.deal(user1, 100 ether);
-        vm.prank(user1);
-        vm.expectRevert("msg.value deos not match price of tokens");
-        sale.buy{value: buyPrice}(tokenAmount + 1);
-    }
-
-    function testBuyAndSell() public {
-        uint256 tokenAmount = 5 ether;
-        uint256 buyPrice = sale.buyPriceCalculation(tokenAmount);
-        console2.log("'buyPrice' for 5 tokens is %s", buyPrice);
-
-        console2.log("'cooldown': %s", sale.cooldown(user1));
 
         vm.startPrank(user1);
-        vm.deal(user1, 100 ether);
         sale.buy{value: buyPrice}(tokenAmount);
-
-        console2.log("'cooldown': %s", sale.cooldown(user1));
-
-        console2.log("user1 bought 5 tokens");
-        console2.log("Current eth balance of contract: %s", address(sale).balance);
-        console2.log("'currentPrice': %s", sale.currentPrice() / 10 ** 18);
+        assertEq(address(sale).balance, buyPrice);
+        assertEq(sale.currentPrice(), 6 ether);
 
         buyPrice = sale.buyPriceCalculation(tokenAmount);
-        console2.log("'buyPrice' for 5 more tokens (from 5 to 10) is %s", buyPrice);
+        assertEq(buyPrice, 85 ether / 2);
+        uint256 balance = address(sale).balance;
 
-        console2.log("'cooldown': %s", sale.cooldown(user1));
-
-        uint256 blockNumber = block.number;
-        vm.roll(blockNumber + 5);
-
-        vm.deal(user1, 100 ether);
         sale.buy{value: buyPrice}(tokenAmount);
-        console2.log("user1 buys 5 more tokens");
-        console2.log("Current eth balance of contract: %s", address(sale).balance);
-        assertEq(address(sale).balance, 60 ether);
-        console2.log("Current 'totalSupply' : %s", sale.totalSupply() / 10 ** 18);
-
-        vm.expectRevert("Cooldown period has not expired yet");
-        sale.transferAndCall(address(sale), 5 ether, bytes(""));
-
-        blockNumber = block.number;
-        vm.roll(blockNumber + 5);
-        sale.transferAndCall(address(sale), 5 ether, bytes(""));
-        console2.log("user1 sends 5 tokens to 'sale' (automatic sell)");
-        console2.log("Current eth balance of contract: %s", address(sale).balance);
-        console2.log("Current 'totalSupply': %s", sale.totalSupply() / 10 ** 18);
+        assertEq(address(sale).balance, balance + buyPrice);
+        assertEq(sale.totalSupply(), tokenAmount * 2);
+        assertEq(sale.balanceOf(user1), tokenAmount * 2);
+        vm.stopPrank();
     }
 
     function testBuyWithDecimals() public {
         uint256 tokenAmount = 5 ether;
         uint256 buyPrice = sale.buyPriceCalculation(tokenAmount);
-        console2.log("'buyPrice' for 5 tokens is %s", buyPrice);
-        vm.prank(user1);
-        vm.deal(user1, 100 ether);
-        sale.buy{value: buyPrice}(tokenAmount);
 
-        uint256 blockNumber = block.number;
-        vm.roll(blockNumber + 5);
+        vm.prank(user1);
+        sale.buy{value: buyPrice}(tokenAmount);
 
         tokenAmount = 4.9 ether;
         buyPrice = sale.buyPriceCalculation(tokenAmount);
-        console2.log("'buyPrice' for 4.9 tokens is %s", buyPrice);
+
         vm.prank(user2);
-        vm.deal(user2, 100 ether);
         sale.buy{value: buyPrice}(tokenAmount);
 
         tokenAmount = 0.1 ether;
         buyPrice = sale.buyPriceCalculation(tokenAmount);
-        console2.log("'buyPrice' for 0.1 tokens is %s", buyPrice);
+
         vm.prank(user2);
-        vm.deal(user2, 100 ether);
-
-        vm.expectRevert("Cooldown period has not expired yet");
-        blockNumber = block.number;
-        vm.roll(blockNumber + 4);
         sale.buy{value: buyPrice}(tokenAmount);
 
-        blockNumber = block.number;
-        vm.roll(blockNumber + 1);
-        sale.buy{value: buyPrice}(tokenAmount);
-
-        console2.log("Current eth balance of contract: %s", address(sale).balance / 10 ** 18);
-        console2.log("Current 'totalSupply': %s", sale.totalSupply() / 10 ** 18);
-        console2.log("'currentPrice': %s", sale.currentPrice() / 10 ** 18);
+        assertEq(address(sale).balance, 60 ether);
+        assertEq(sale.totalSupply(), 10 ether);
+        assertEq(sale.balanceOf(user1), sale.balanceOf(user2));
     }
 
-    function testTotalMarketValue() public {
-        uint256 tokenAmount = 1_000_000 ether; // a million tokens
+    function testBuyFail() public {
+        uint256 tokenAmount = 10 ether;
+        uint256 ethRequired = sale.buyPriceCalculation(tokenAmount); // 10 tokens -> 60 ether as a depostit
+
+        vm.prank(user1);
+        vm.expectRevert("Actual amount of tokens is less than expected minimum");
+        sale.buy{value: ethRequired - 1}(tokenAmount);
+    }
+
+    function testSell() public {
+        uint256 tokenAmount = 10 ether;
         uint256 buyPrice = sale.buyPriceCalculation(tokenAmount);
-        console2.log("'buyPrice' for 1_000_000 tokens is %s", buyPrice / 10 ** 18);
+
+        vm.startPrank(user1);
+        sale.buy{value: buyPrice}(tokenAmount);
+
+        uint256 balance = address(sale).balance;
+        uint256 salePrice = sale.sellPriceCalculation(tokenAmount / 2);
+
+        sale.transferAndCall(address(sale), tokenAmount / 2, abi.encode(salePrice));
+        assertEq(address(sale).balance, balance - salePrice);
+        assertEq(sale.totalSupply(), tokenAmount / 2);
+        assertEq(sale.balanceOf(user1), tokenAmount / 2);
+        vm.stopPrank();
+    }
+
+    function testSellFail() public {
+        uint256 tokenAmount = 10 ether;
+        uint256 buyPrice = sale.buyPriceCalculation(tokenAmount);
+
+        vm.startPrank(user1);
+        sale.buy{value: buyPrice}(tokenAmount);
+
+        uint256 salePrice = sale.sellPriceCalculation(tokenAmount / 2);
+
+        vm.expectRevert("Actual amount of ether is less than expected minimum");
+        sale.transferAndCall(address(sale), tokenAmount / 2, abi.encode(salePrice + 1));
+        vm.stopPrank();
+    }
+
+    function testBuyAndSellFailedTransfers() public {
+        vm.deal(address(this), 100 ether);
+        assertEq(address(this).balance, 100 ether);
+
+        uint256 tokenAmount = 10 ether;
+        uint256 buyPrice = sale.buyPriceCalculation(tokenAmount);
+
+        vm.expectRevert("Unsuccessful call");
+        sale.buy{value: buyPrice + 1}(tokenAmount);
+
+        sale.buy{value: buyPrice}(tokenAmount);
+
+        vm.expectRevert("Unsuccessful call");
+        sale.transferAndCall(address(sale), 1, abi.encode(1));
+    }
+
+    receive() external payable {
+        if (msg.value > 0) revert();
     }
 }
